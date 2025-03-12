547d8f7f727428b70100e452e07caeb96b449d5e
Novv <73749711+Novavalentino@users.noreply.github.com>
2025-03-12 11:10:43 +0700

    Add files via upload

---
+++ ServiceOrientedArchitecture/pert2/books.js
@@ -0,0 +1,30 @@
+const http = require('http');
+
+const server = http.createServer((req,res) => {
+    if (req.url === '/') {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/plain');
+        res.end("Hello World");    
+    }else if (req.url === '/books') {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/plain');
+        res.end("Selamat datang di aplikasi informasi buku");
+    }else if (req.url === '/books/1') {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/plain');
+        res.end("Buku 1: Laut Bercerita\nPenulis: Lella S.Chudorl");
+    }else if (req.url === 'books/2') {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/plain');
+        res.end("Buku 2: The Lord Of The Ring\nPenulis: J.R.R Tolken");
+    }else {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/plain');
+        res.end("Halaman tidak ditemukan");
+    }
+    
+});
+
+server.listen(3000, 'localhost', ()=> {
+    console.log('server berjalan pada http://localhost:3000/')
+});
